c73e21ca09dfcf52eb16cc99781c7baf0589a003
fgreyhhj <psjcinquibln456@hotmail.com>
2024-05-09 21:08:10 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-}
-function isValid(node, min, max) {
-  if (!node) return true;
-  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-    return false;
-  return (
-    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-  );
-}
+const word = "hello";
+const reversedWord = word.split("").reverse().join("");
+console.log(reversedWord); // olleh
