bd1bdfa2708503bd9c59127af47112ec4f291b3d
fgreyhhj <psjcinquibln456@hotmail.com>
2024-05-11 05:34:47 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const word = "hello";
-const reversedWord = word.split("").reverse().join("");
-console.log(reversedWord); // olleh
+function reverseBetween(head, m, n) {
+  if (!head || m === n) return head;
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+  for (let i = 0; i < m - 1; i++) prev = prev.next;
+  let start = prev.next;
+  let then = start.next;
+  for (let i = 0; i < n - m; i++) {
+    start.next = then.next;
+    then.next = prev.next;
+    prev.next = then;
+    then = start.next;
+  }
+  return dummy.next;
+}
