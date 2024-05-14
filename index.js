2eabe5a82ed4677c4c089939ec0c3265b2d6bae6
fgreyhhj <psjcinquibln456@hotmail.com>
2024-05-15 06:20:28 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function canJump(nums) {
-  let maxJump = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (i > maxJump) return false;
-    maxJump = Math.max(maxJump, i + nums[i]);
-    if (maxJump >= nums.length - 1) return true;
-  }
-  return false;
-}
+const name = "john";
+const upperName = name.toUpperCase();
+console.log(upperName); // JOHN
